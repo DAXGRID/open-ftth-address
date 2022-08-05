ded7d67c183f5de4c4fdb2c81dbb1519d0476404
Rune Nielsen <runenielsen@runbox.com>
2022-08-05 18:16:24 +0200

    improves formatting

--- test/OpenFTTH.Core.Address.Tests/Startup.cs
+++ test/OpenFTTH.Core.Address.Tests/Startup.cs
@@ -11,7 +11,8 @@ public static class Startup
     {
         services.AddSingleton<IEventStore, InMemEventStore>();
         {
-            var businessAssemblies = new Assembly[] {
+            var businessAssemblies = new Assembly[]
+            {
                 AppDomain.CurrentDomain.Load("OpenFTTH.Core.Address"),
             };
 
