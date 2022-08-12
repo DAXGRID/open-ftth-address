f032a9c334aa532a4ea03018f2ada1942f46d8a9
Rune Nielsen <runenielsen@runbox.com>
2022-08-12 16:40:23 +0200

    now sets created and updated dates on postcode (#63)

--- src/OpenFTTH.Core.Address/Events/PostCodeDeleted.cs
+++ src/OpenFTTH.Core.Address/Events/PostCodeDeleted.cs
@@ -3,9 +3,11 @@ namespace OpenFTTH.Core.Address.Events;
 public sealed record PostCodeDeleted
 {
     public Guid Id { get; init; }
+    public DateTime Updated { get; init; }
 
-    public PostCodeDeleted(Guid id)
+    public PostCodeDeleted(Guid id, DateTime updated)
     {
         Id = id;
+        Updated = updated;
     }
 }
