1750698392c6543fb2ef52c2991a9cf35ff55e92
Rune Nielsen <runenielsen@runbox.com>
2022-08-26 20:10:34 +0200

    fixes incorrect external updated naming and nullability (#72)

--- src/OpenFTTH.Core.Address/Events/AccessAddressDeleted.cs
+++ src/OpenFTTH.Core.Address/Events/AccessAddressDeleted.cs
@@ -3,9 +3,9 @@ namespace OpenFTTH.Core.Address.Events;
 public sealed record AccessAddressDeleted
 {
     public Guid Id { get; init; }
-    public DateTime ExternalUpdatedDate { get; init; }
+    public DateTime? ExternalUpdatedDate { get; init; }
 
-    public AccessAddressDeleted(Guid id, DateTime externalUpdatedDate)
+    public AccessAddressDeleted(Guid id, DateTime? externalUpdatedDate)
     {
         Id = id;
         ExternalUpdatedDate = externalUpdatedDate;
